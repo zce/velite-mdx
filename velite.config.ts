646667660ce232e4dd48edfec6f073f8245c273a
zce <w@zce.me>
2023-11-22 14:45:44 +0800

    feat: update velite

--- velite.config.ts
+++ velite.config.ts
@@ -1,35 +1,18 @@
 import { defineConfig, s } from 'velite'
 
-const slugify = (input: string) =>
-  input
-    .toLowerCase()
-    .replace(/\s+/g, '-')
-    .replace(/[^a-z0-9-]/g, '')
-
-const icon = s.enum(['github', 'instagram', 'medium', 'twitter', 'youtube'])
-const count = s.object({ total: s.number(), posts: s.number() }).default({ total: 0, posts: 0 })
-
-const meta = s
-  .object({
-    title: s.string().optional(),
-    description: s.string().optional(),
-    keywords: s.array(s.string()).optional()
-  })
-  .default({})
-
 export default defineConfig({
   root: 'content',
   output: {
     data: '.velite',
     static: 'public',
-    filename: '/static/[name]-[hash:6].[ext]'
+    filename: '/static/[name]-[hash:6].[ext]',
+    clean: true
   },
-  clean: true,
-  schemas: {
+  collections: {
     pages: {
       name: 'Page',
       pattern: 'pages/**/*.mdx',
-      fields: s.object({
+      schema: s.object({
         title: s.string().max(99),
         slug: s.slug(),
         description: s.string(),
@@ -39,7 +22,7 @@ export default defineConfig({
     posts: {
       name: 'Post',
       pattern: 'posts/**/*.mdx',
-      fields: s.object({
+      schema: s.object({
         title: s.string().max(99),
         slug: s.slug(),
         description: s.string(),
